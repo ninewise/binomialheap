4264ba7b64c8fb9ff8cdf37449750d05ea5bae7d
Felix Van der Jeugt <felix.vanderjeugt@gmail.com>
2012-01-13 21:32:52 +0100

    rubbish

--- BinomialHeap.h
+++ BinomialHeap.h
@@ -2,6 +2,7 @@
 #ifndef BINOMIAL_HEAP_H
 #define BINOMIAL_HEAP_H
 
+<<<<<<< HEAD
 namespace integered {
 
     class Element {
@@ -28,6 +29,58 @@ namespace integered {
         Element* large;
 
     }
+=======
+/**
+ * Contains an implementation of Binomial Heaps of integers.
+ */
+namespace integered {
+
+    /**
+     * A Binomial Tree.
+     */
+    class Tree {
+      
+        public:
+        // Create a default tree of size 0.
+        Tree();
+
+        // Create a new Tree with size, value and no childeren.
+        Tree(int size, int value);
+
+        // Copy constructor.
+        Tree(const Tree& tree);
+
+        // Destructor.
+        virtual ~Tree();
+
+        int getValue() const;
+        int getSize() const;
+
+        void setValue(int value);
+
+        // Deep copy of tree. Needs the same size.
+        Tree& operator=(const Tree& tree);
+
+        // Check if the size, value and (recursively) childeren are equal.
+        bool operator==(const Tree& tree) const;
+
+        // The merge of two tree of the same size.
+        friend const Tree operator+(const Tree& tree1, const Tree& Tree2);
+
+        // Print the binomial tree.
+        void print() const;
+        void printr(int prefix) const;
+
+        private:
+        // The size of the tree. This is the order of the tree.
+        const int size;
+        // The value of the root node.
+        int value;
+        // An array of the childeren of this Trees root.
+        Tree* childeren;
+
+    };
+>>>>>>> parent of 6f4eb09... cleanup for restart
 
 }
 
--- main.cpp
+++ main.cpp
@@ -1,9 +1,6 @@
 
 #include "BinomialHeap.h"
-using namespace integered;
-
-#include <iostream>
-using namespace std;
 
 int main() {
+    
 }
